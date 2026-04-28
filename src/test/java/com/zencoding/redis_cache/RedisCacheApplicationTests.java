9dbb6a3556bf6be1c559be272e3418c2483964e5
Muhammad Zein Fikry Lazuardi <zenfikry@gmail.com>
2026-04-28 13:57:32 +0700

    initial commit

---
+++ src/test/java/com/zencoding/redis_cache/RedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zencoding.redis_cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
