0ba3d609ad25026a0685ade831e22d1a9da1b6ed
zenfikry <zenfikry@gmail.com>
2026-05-19 15:42:01 +0700

    optimize caching with redis

--- src/main/java/com/zencoding/redis_cache/service/CategoryService.java
+++ src/main/java/com/zencoding/redis_cache/service/CategoryService.java
@@ -4,8 +4,12 @@ import com.zencoding.redis_cache.entity.Category;
 import com.zencoding.redis_cache.model.CategoryResponse;
 import com.zencoding.redis_cache.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 
+import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -14,10 +18,22 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private final ObjectMapper objectMapper;
+
     public List<CategoryResponse> findAll() {
+
+        String json = stringRedisTemplate.opsForValue().get("categories");
+
+        if (json != null) {
+            return objectMapper.readValue(json, new TypeReference<>() {
+            });
+        }
+
         List<Category> parents = categoryRepository.findAllCategories();
 
-        return parents.stream().map(category -> {
+        List<CategoryResponse> responses = parents.stream().map(category -> {
 
             List<Category> children = category.getChildren();
             List<CategoryResponse> childrenResponses = children.stream().map(child -> {
@@ -26,5 +42,10 @@ public class CategoryService {
 
             return CategoryResponse.builder().id(category.getId()).name(category.getName()).children(childrenResponses).build();
         }).toList();
+
+        json = objectMapper.writeValueAsString(responses);
+        stringRedisTemplate.opsForValue().set("categories", json, Duration.ofDays(1) );
+
+        return responses;
     }
 }
